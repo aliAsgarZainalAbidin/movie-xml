c2fdc398d5c0586da6f2bfdd53887d042ad27a76
Ali Asgar Zainal Abidin <49857639+aliAsgarZainalAbidin@users.noreply.github.com>
2021-09-06 10:59:58 +0800

    Log Changes : 1. impelemen SplashScreenActivity.kt 2. add navigation component to project

--- app/src/main/java/com/example/movie_app_xml/view/MainActivity.kt
+++ app/src/main/java/com/example/movie_app_xml/view/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.movie_app_xml.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.movie_app_xml.R
+import com.example.movie_app_xml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = supportFragmentManager.findFragmentById(R.id.fcv_main_container)?.findNavController() as NavController
+        binding.botnavMainContainer.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.overview -> {
+                    navController.navigate(R.id.overviewFragment)
+                    true
+                }
+                else -> true
+            }
+        }
     }
+
 }
--- app/src/main/java/com/example/movie_app_xml/view/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/example/movie_app_xml/view/splash/SplashScreenActivity.kt
@@ -1,12 +1,22 @@
 package com.example.movie_app_xml.view.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.movie_app_xml.R
+import com.example.movie_app_xml.view.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+            this.startActivity(intent)
+            finish()
+        },1500)
     }
 }
